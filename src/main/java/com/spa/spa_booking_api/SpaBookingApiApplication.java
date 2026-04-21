6f5573bbd2e879d99c19d5ec0e68e8cf7049d76d
firpen <firrpen@hotmail.com>
2026-04-21 13:59:32 +0200

    initial commit

---
+++ src/main/java/com/spa/spa_booking_api/SpaBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.spa.spa_booking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaBookingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spa/spa_booking_api/SpaBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spa.spa_booking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
